0c8caf448892a7e6a25167e105618b3ae5a4c08e
kmgill <kgill@jpl.nasa.gov>
2017-12-28 12:53:50 -0800

    Added additional camera options; Handler now holds a single camera instance, will need to add a mutex to prevent parallel requests

--- src/webservice/handlers/StillImageHandler.py
+++ src/webservice/handlers/StillImageHandler.py
@@ -22,31 +22,39 @@ class StillImageHandlerImpl(BaseHandler):
     def __init__(self):
         BaseHandler.__init__(self)
 
+        self.camera = picamera.PiCamera()
+
     def handle(self, computeOptions, **args):
 
         vflip = computeOptions.get_boolean_arg("vflip", False)
         hflip = computeOptions.get_boolean_arg("hflip", False)
         iso = computeOptions.get_int_arg("iso")
         shutter_speed = computeOptions.get_int_arg("ss")
-        settle_time = computeOptions.get_int_arg("st", 2)
+        settle_time = computeOptions.get_int_arg("st", 0)
         hres = computeOptions.get_int_arg("hres", 1280)
         vres = computeOptions.get_int_arg("vres", 720)
+        exposure_mode = computeOptions.get_argument("ex")
+        awb_mode = computeOptions.get_argument("awb")
 
         bytes = BytesIO()
-        with picamera.PiCamera(resolution=(hres, vres),framerate=Fraction(1, 6), sensor_mode=3) as camera:
+        #with picamera.PiCamera(resolution=(hres, vres),framerate=Fraction(1, 6), sensor_mode=3) as camera:
 
-            camera.hflip = vflip
-            camera.vflip = hflip
+        self.camera.hflip = vflip
+        self.camera.vflip = hflip
+        self.camera.resolution = (hres, vres)
+        if iso is not None:
+            self.camera.iso = iso
+        if shutter_speed is not None:
+            self.camera.shutter_speed = shutter_speed
+        if exposure_mode is not None:
+            self.camera.exposure_mode = exposure_mode
+        if awb_mode is not None:
+            self.camera.awb_mode = awb_mode
 
-            if iso is not None:
-                camera.iso = iso
-            if shutter_speed is not None:
-                camera.shutter_speed = shutter_speed
+        if settle_time is not None:
+            sleep(settle_time)
 
-            if settle_time is not None:
-                sleep(settle_time)
-
-            camera.capture(bytes, format='jpeg')
+        self.camera.capture(bytes, format='jpeg')
 
         class SimpleResult(object):
             def __init__(self, result):
